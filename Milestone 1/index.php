8e2aa54dc1c5d9f4418383c75048c803fc892ba1
Loris Matteri <loris.matteri93@gmail.com>
2021-02-02 13:07:52 +0100

    Primo commit
    creato file php con database degli album, usato include per richiamarlo nell'index.php. con foreach verificato che funzioni

---
+++ Milestone 1/index.php
@@ -0,0 +1,25 @@
+<?php
+    include __DIR__ . './data.php';
+
+    foreach ($albums as $album) {
+        echo $album["title"] . "<br>";
+        echo $album["author"] . "<br>";
+        echo $album["genre"] . "<br>";
+        echo $album["year"] . "<br>";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dischi</title>
+    </head>
+    <body>
+
+        <h1>ciao</h1>
+
+        
+    </body>
+</html>
